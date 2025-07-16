9d90305c902794827a2070c95a6982b8fc95fa24
Timo 'Timii' Paananen <paananen.t@gmail.com>
2025-07-17 00:43:31 +0300

    emmit

--- temp.cpp
+++ temp.cpp
@@ -35,8 +35,21 @@ TEMP::~TEMP()
 }
 QString TEMP::getTEMP()
 {
-    qDebug() <<" seen " << TEMP::temp;
-    return  TEMP::temp;
+    QFile file("/sys/bus/w1/devices/28-01193a3eb5b5/w1_slave"); // Vaihda oikeaan ID:hen
+    if (!file.open(QIODevice::ReadOnly | QIODevice::Text))
+        return "Error";
+
+    QTextStream in(&file);
+    QString content = in.readAll();
+    file.close();
+
+    int index = content.indexOf("t=");
+    if (index != -1) {
+        QString tempString = content.mid(index + 2);
+        double tempC = tempString.toInt() / 1000.0;
+        return QString::number(tempC, 'f', 2) + " °C";
+    }
+    return "Invalid data";
 }
 void TEMP::setTEMP(QString temp)
 {
@@ -46,7 +59,26 @@ void TEMP::setTEMP(QString temp)
 
 }
 
+QString readTEMP() {
+    QFile file("/sys/bus/w1/devices/28-01193a3eb5b5/w1_slave"); // Vaihda oikeaan ID:hen
+    if (!file.open(QIODevice::ReadOnly | QIODevice::Text))
+        return "Error";
+
+    QTextStream in(&file);
+    QString content = in.readAll();
+    file.close();
+
+    int index = content.indexOf("t=");
+    if (index != -1) {
+        QString tempString = content.mid(index + 2);
+        double tempC = tempString.toInt() / 1000.0;
+        return QString::number(tempC, 'f', 2) + " °C";
+    }
+    return "Invalid data";
+}
+
 void TEMP::updateTEMP()
 {
+    setTEMP(readTEMP());
 
 }
