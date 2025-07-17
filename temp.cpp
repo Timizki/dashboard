8f6356da0cb9a605bdf272c7df067a8299bcce81
Timo 'Timii' Paananen <paananen.t@gmail.com>
2025-07-17 22:35:55 +0300

    removed stuff

--- temp.cpp
+++ temp.cpp
@@ -5,28 +5,7 @@
 TEMP::TEMP(QObject *parent)
     : QObject{parent}
 {
-    QObject::connect(&temperature, &QFile::readyRead, [this]() {
 
-        QTextStream in(&temperature);
-        QString content = in.readAll();
-        temperature.close();
-        QString data;
-        int index = content.indexOf("t=");
-        if (index != -1) {
-            QString tempString = content.mid(index + 2);
-            double tempC = tempString.toInt() / 1000.0;
-            data = QString::number(tempC, 'f', 2) + " °C";
-        }
-        if(data != nullptr)   {
-            setTEMP(data);
-            qDebug() << "Received:" << data;
-        }
-           qDebug() << "NO temp data received";
-    });
-
-    QTimer *timer = new QTimer(this);
-    connect(timer, &QTimer::timeout, this,  &TEMP::updateTEMP);
-    timer->start(1000);
 }
 
 TEMP::~TEMP()
